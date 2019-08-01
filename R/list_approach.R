59d47a8416c335d4b19c2398846ae7c3c0e0c804
vroni-g <veronika.grupp@posteo.de>
2019-08-01 22:39:38 +0200

    basic version works, Absätze stimmen noch nicht ganz

--- R/list_approach.R
+++ R/list_approach.R
@@ -1,48 +1,54 @@
 library(tidyverse)
 
 #*******************
-# funktioniert noch nicht in der schleife, wenn sozusagen subfield wieder als 
-# Parameter für query_builder übergeben wird, das ist dann type 'closure'...
-# Es stimmen auch noch nicht alle eckigen Klammern
-# Muss jetzt aber Abendessen ;-)
 
-query_builder <- function(field) {
+query_builder <- function(field, substat_name){
+  
+  if(field$name == substat_name){ # stop recursive function on substat-level
+    glue::glue('year, <<substat_name>>', .open = "<<", .close = ">>") # preliminary
+  } else {
+  
   # check for field arguments
   if (length(field$arguments) > 1) {
     a <- lapply(field[['arguments']], paste_nv) %>%
       unlist(.) %>%
-      paste0(., collapse = ', ') %>%
-      paste0('(', ., ')', collapse = ' ')
-  } else {
-    if (length(field$arguments) == 1) {
-      a <- paste_nv(field[['arguments']]) %>%
-        paste0(., collapse = ', ') %>%
-        paste0('(', ., ')', collapse = ' ')
-    } else {
+      paste0(., collapse = ', ')
+    a <- glue::glue('( <<a>> )', .open = "<<", .close = ">>")
+  }
+  if (length(field$arguments) == 1) {
+    a <- field[['arguments']][[1]] %>%
+        paste_nv(.) %>% 
+        paste0(., collapse = ', ') 
+    a <- glue::glue('( <<a>> )', .open = "<<", .close = ">>")
+  } 
+  if (length(field$arguments) == 0) {
       a <- NULL
     }
-  }
-  
   
   # check for subfields
   if (length(field$subfield) == 0) {
-    query <- paste0(field$name, a, '{ }', collapse = ' ')
+    query <- glue::glue('<<field$name>> <<a>> { }', .open = "<<", .close = ">>")
   } else {
-    paste0(field$name,
-          a,
-          '{',
-          paste0(purrr::map_chr(field$subfield, query_builder), collapse = ' '),
-          '}')
+    subfield <- field$subfield
+    glue::glue('<<field$name>>
+          <<a>>
+          {
+          <<glue::glue("<<purrr::map_chr(subfield, query_builder, substat_name = substat_name)>>", .open = "<<", .close = ">>")>>
+          }', .open = "<<", .close = ">>")
   }
 
+ }
 }
-
 
 #*******************
 
 paste_nv <- function(field){
-  paste(field[['name']], ':',
-        field[['value']])
+  if(field$type == 'String'){
+    value <- glue::glue('"<<field$value>>"', .sep = ' ', .open = "<<", .close = ">>")
+    glue::glue('<<field[["name"]]>> : <<value>>', .sep = ' ', .open = "<<", .close = ">>")
+  } else {
+    glue::glue('<<field[["name"]]>> : <<field[["value"]]>>', .sep = ' ', .open = "<<", .close = ">>")
+  }
 }
 
 
@@ -56,35 +62,41 @@ substat_name <- 'BAUNW2'
 parameter <- 'BAUNW101'
 year <- 2002
 
-region <- list('name' = 'region',
-               'value' = list(),
-               'arguments' = list(id),
-               'subfield' = list(stat))
-
-id <-list('name' = 'id',
-          'value' = region_id,
-          'arguments' = list(),
-          'subfield'= list())
-
-stat <- list('name' = stat_name, 
-             'value' = list(),
-             'arguments' = list(year, substat),
-             'subfield'= list(substat))
-
+substat <- list('name' = substat_name,
+                'value' = parameter,
+                'arguments' = list(),
+                'subfield'= list(),
+                'type' = substat_name)
 
 year <- list('name' = 'year',
              'value' = year,
              'arguments' = list(),
-             'subfield'= list())
+             'subfield'= list(),
+             'type' = 'Int')
 
-substat <- list('name' = substat_name,
-                'value' = parameter,
-                'arguments' = list(),
-                'subfield'= list())
+stat <- list('name' = stat_name, 
+             'value' = list(),
+             'arguments' = list(year, substat),
+             'subfield'= list(substat),
+             'type' = stat_name)
 
-query <- query_builder(region)
-stat_query <- query_builder(substat)
+id <-list('name' = 'id',
+          'value' = region_id,
+          'arguments' = list(),
+          'subfield'= list(),
+          'type' = 'String')
 
+
+region <- list('name' = 'region',
+               'value' = list(),
+               'arguments' = list(id),
+               'subfield' = list(stat),
+               'type' = 'Region')
+
+
+
+query <- query_builder(field = region, substat_name = 'BAUNW2')
+# region id not quoted....
 
 ####################
 
