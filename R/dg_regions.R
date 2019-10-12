de127ac94863c6e328f9861553d39f807f5c02d1
lhehnke <lisa@dataplanes.org>
2019-10-12 10:32:38 +0200

    Add optional filter to dg_regions

--- R/dg_regions.R
+++ R/dg_regions.R
@@ -1,12 +1,25 @@
 #### This code imports information on all regions ####
 
-get_regions <- function() {
-  df <- read_csv("data/regions.csv")
+## Option to filter by variable (e.g. level or "level") and value (e.g. "lau").
+
+get_region = function(var = NULL, val = NULL) {
+  
+  if (missing(var) | missing(val)) { 
+    df <- read_csv("data/regions.csv")
+    
+  } else { 
+    
+    var <- rlang::parse_expr(quo_name(enquo(var)))
+    
+    df <- read_csv("data/regions.csv") %>% 
+      filter(!!var == val)  
+  }
+  
   return(df)
 }
 
 
-dg_regions <- get_regions()
+dg_regions <- get_region()
 
 
 usethis::use_data(dg_regions, overwrite = T)
