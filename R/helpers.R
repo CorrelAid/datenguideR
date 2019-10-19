3f4f0bcb4870c6f3d5b424196907fb2fd7e9d3d8
Lisa Hehnke <lisa@dataplanes.org>
2019-10-19 09:14:48 +0000

    Update helpers.R

--- R/helpers.R
+++ R/helpers.R
@@ -76,7 +76,7 @@ clean_it <- function(.data) {
     ##TODO: Rename name in source sub dataset because there is already a "name" variable in main
     ##NOTE Lisa: Those two vars should not be overall source, but rather source for e.g. different params in TIERA8!
     dplyr::bind_cols(raw$source) %>%
-    dplyr::rename(GENESIS_source = "title_de", GENESIS_source_nr = "name1")
+    dplyr::rename_all(dplyr::recode, title_de = "GENESIS_source", name1 = "GENESIS_source_nr")
     
   return(tidy_dat)
 }
