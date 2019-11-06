05b5e1f23c9b547443e7523709ad8b4dd2a806ba
Daniel Possenriede <possenriede@gmail.com>
2019-11-06 21:46:45 +0100

    expect dg_regions$id to be unique

--- tests/testthat/test-dg_regions.R
+++ tests/testthat/test-dg_regions.R
@@ -2,6 +2,10 @@ test_that("dg_regions$id is of type 'character'", {
   expect_type(dg_regions[["id"]], "character")
 })
 
+test_that("dg_regions$id is of type 'character'", {
+  expect_identical(dg_regions[["id"]], unique(dg_regions[["id"]]))
+})
+
 test_that("get_region returns subset of dg_regions", {
   r1 <- get_region(var = level, val = "nuts1")
   expect_s3_class(r1, c("tbl_df", "tbl", "data.frame"))
