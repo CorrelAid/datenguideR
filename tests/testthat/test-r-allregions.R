0efab888084a986aec342f6cf9e54ce0769c9ea0
favstats <fabio.votta@gmail.com>
2019-11-08 10:05:25 +0100

    change tests

--- tests/testthat/test-r-allregions.R
+++ tests/testthat/test-r-allregions.R
@@ -1,30 +1,39 @@
 test_that("calling number of pigs works nuts 1", {
   
-  dg_call(year = c(2001, 2003, 2007), 
+  result <- dg_call(year = c(2001, 2003, 2007), 
           stat_name = 'BETR08', 
           substat_name = 'TIERA8', 
           nuts_nr = 1) 
+  
+  expect_equal(nrow(result), 383)
   
   
 })
 
 test_that("calling number of pigs works nuts 2", {
   
-  dg_call(year = c(2001, 2003, 2007), 
+  result <- dg_call(year = c(2001, 2003, 2007), 
           stat_name = 'BETR08', 
           substat_name = 'TIERA8', 
           nuts_nr = 2) 
+  
+  expect_equal(nrow(result), 744)
   
   
 })
 
 test_that("calling number of pigs works nuts 3", {
   
-  dg_call(year = c(2001, 2003, 2007),
+  result <- dg_call(year = c(2001, 2003, 2007),
           stat_name = 'BETR08',
           substat_name = 'TIERA8',
           nuts_nr = 3,
           parent_chr = "1")
+  
+  
+  
+  expect_equal(nrow(result), 2901)
+  
   
   
 })
@@ -32,9 +41,13 @@ test_that("calling number of pigs works nuts 3", {
 
 test_that("Nichtwohngebäude works", {
   
-  dg_call(nuts_nr = 1, 
+  result <- dg_call(nuts_nr = 1, 
           stat_name =  "BAU018", 
           substat_name = "BAUAHZ")
+  
+  
+  expect_equal(nrow(result), 224)
+  
   
   
 })
--- tests/testthat/test-r-region.R
+++ tests/testthat/test-r-region.R
@@ -1,26 +1,33 @@
 test_that("calling a simple statistic works", {
   
-  dg_call(region_id = "11",
+  result <- dg_call(region_id = "11",
           year = 2002,
           stat_name = 'AI0506')
+  
+  expect_equal(nrow(result), 1)
   
 })
 
 test_that("calling a substat statistic works", {
   
-  dg_call(region_id = "11", 
+  result <- dg_call(region_id = "11", 
           year = c(2001, 2003, 2007), 
           stat_name = 'BETR08', 
           substat_name = 'TIERA8') 
+  
+  expect_equal(nrow(result), 23)
   
 })
 
 test_that("calling a substat statistic with parameters works", {
   
-  dg_call(region_id = "11", 
+  result <- dg_call(region_id = "11", 
           year = c(2001, 2003, 2007), 
           stat_name = 'BETR08', 
           substat_name = 'TIERA8',
           parameter = c("TIERART2", "TIERART3", "TIERART4")) 
+  
+
+  expect_equal(nrow(result), 9)
   
 })
