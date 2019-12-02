8984dd342a35e95391a234074a9bafc9a714965f
Daniel Possenriede <possenriede@analyse-konzepte.de>
2019-12-02 09:24:56 +0100

    get dg_regions from data.genesapi.org

--- data-raw/dg_regions.R
+++ data-raw/dg_regions.R
@@ -1,5 +1,24 @@
-dg_regions <- readr::read_csv("data-raw/regions.csv", col_types = "cccc")
-
-dg_regions <- dplyr::arrange(dg_regions, id)
+library(dplyr)
+x <- 
+  jsonlite::read_json("https://data.genesapi.org/regionalstatistik/names.json")
+dg_regions <- 
+  dplyr::tibble(id = names(x), name = as.character(x)) %>% 
+  dplyr::mutate(
+    level = 
+      dplyr::case_when(
+        id == "DG" ~ "nuts0",
+        nchar(id) == 2 ~ "nuts1",
+        nchar(id) == 3 ~ "nuts2",
+        nchar(id) == 5 ~ "nuts3",
+        nchar(id) == 8 ~ "lau"),
+    parent =
+      dplyr::case_when(
+        nchar(id) == 2 ~ "DG",
+        nchar(id) == 3 ~ substr(id, 1, 2),
+        nchar(id) == 5 ~ substr(id, 1, 3),
+        nchar(id) == 8 ~ substr(id, 1, 5)
+      )
+  ) %>% 
+  dplyr::arrange(id)
 
 usethis::use_data(dg_regions, overwrite = TRUE)
