4555c6ef596985a3ee6482c00bd9a93a7e9b6bcb
favstats <fabio.votta@gmail.com>
2020-01-29 23:41:33 +0100

    add en_list

--- R/dg_descriptions.R
+++ R/dg_descriptions.R
@@ -1,3 +1,31 @@
+##' join_en_translation
+##'
+##' This function joins a list of English translations to the dg_descriptions dataset.
+##'
+##' @param dg_descriptions data set which includes all meta data
+##' @param en_list list that includes all English translations
+##' @return Data frame containing all available meta data *with* English translation
+##'
+##' @examples
+##' dg_descriptions <- join_en_translation(datenguideR::dg_descriptions, en_list)
+##' dg_descriptions
+##'
+##' @export
+join_en_translation <- function(dg_descriptions, en_list) {
+  start_dat <- dg_descriptions
+  
+  for (jj in seq_along(en_list)) {
+    start_dat <- start_dat %>% 
+      dplyr::left_join(en_list[[jj]])
+  }
+  
+  start_dat <- start_dat %>% dplyr::mutate_all(~ifelse(.x == "N / a", NA, .x))
+  
+  return(start_dat)
+}
+
+
+
 ##' dg_descriptions.R
 ##'
 ##' Gets all available meta data on statistics, sub-statistics, and parameters.
@@ -32,26 +60,26 @@ get_descriptions <- function() {
      }
    }
  }}'
-
+  
   meta_results <- httr::POST(
     url = "https://api-next.datengui.de/graphql",
     body = list(query = meta_query),
     encode = "json",
     httr::add_headers(.headers = c("Content-Type" = "application/json"))
   )
-
+  
   result_dat <- httr::content(meta_results, as = "text", encoding = "UTF-8") %>%
     jsonlite::fromJSON()
-
+  
   final <- result_dat[["data"]][["__type"]][["fields"]] %>% tibble::as_tibble()
-
+  
   return(final)
 }
 
 dg_descriptions <- get_descriptions() %>%
   dplyr::select(name, description, args) %>%
   dplyr::mutate(stat_description_full = description %>%
-    stringr::str_trim()) %>%
+                  stringr::str_trim()) %>%
   dplyr::mutate(description = stringr::str_extract(description, '(?<=\\*\\*)[^*]*(?=\\*\\*)')) %>%
   utils::tail(-2) %>%
   dplyr::rename_all(dplyr::recode, name = "stat_name", description = "stat_description") %>%
@@ -72,5 +100,15 @@ dg_descriptions <- get_descriptions() %>%
     .vars = c("param_name", "param_description"),
     .funs = list(~ ifelse(substat_name == "", NA, as.character(.)))
   )
+
+load("r/sysdata.rda")
+
+
+dg_descriptions <- join_en_translation(dg_descriptions, en_list)
+
+# load("r/sysdata.rda")
+# usethis::use_data(en_list, overwrite = TRUE, internal = T)
+
+
 
 usethis::use_data(dg_descriptions, overwrite = TRUE)
