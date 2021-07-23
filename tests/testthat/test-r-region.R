e204d3ebbca1e84a35be3bd27cf4cfd6f3b27fd3
lhehnke <lisa@dataplanes.org>
2021-07-23 10:33:50 +0200

    Add workaround for rlang & sf

--- tests/testthat/test-r-region.R
+++ tests/testthat/test-r-region.R
@@ -1,26 +1,29 @@
 test_that("calling a simple statistic works", {
-  dg_call(
+  r4_df1 <- dg_call(
     region_id = "11",
     year = 2002,
-    stat_name = "AI0506"
-  )
+    stat_name = "AI0506")
+  expect_s3_class(r4_df1, c("tbl_df", "tbl", "data.frame"))
+  
 })
 
 test_that("calling a substat statistic works", {
-  dg_call(
+  r4_df2 <- dg_call(
     region_id = "11",
     year = c(2001, 2003, 2007),
     stat_name = "BETR08",
-    substat_name = "TIERA8"
-  )
+    substat_name = "TIERA8")
+  expect_s3_class(r4_df2, c("tbl_df", "tbl", "data.frame"))
+  
 })
 
 test_that("calling a substat statistic with parameters works", {
-  dg_call(
+  r4_df3 <- dg_call(
     region_id = "11",
     year = c(2001, 2003, 2007),
     stat_name = "BETR08",
     substat_name = "TIERA8",
-    parameter = c("TIERART2", "TIERART3", "TIERART4")
-  )
+    parameter = c("TIERART2", "TIERART3", "TIERART4"))
+  expect_s3_class(r4_df3, c("tbl_df", "tbl", "data.frame"))
+  
 })
